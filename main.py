dffb906506ae86fe4c67870ea69f66150d52540d
piachuPL <piasecznyp@gmail.com>
2019-03-14 21:15:10 +0100

    adding sunset function and in GPIO change LOW to HIGH and HIGH to LOW

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import datetime
 import logging
 import paho.mqtt.client as mqtt
 from secrets import *                                   #import secret passwords :D
+import requests
+import bs4 as bs 
+
+HH = 21
+MM = 00                               
 
 
 ### Loging 
@@ -44,11 +49,11 @@ def watering():
         print('otwarcie gpio :'+ str(x[1]))             # printing 'opening GPIO Port
         logging.info('otwieram : '+ str(x[0]))          #log
         GPIO.setup(x[1], GPIO.OUT)                      #set GPIO
-        GPIO.output(x[1], GPIO.HIGH)                    #Set 1
+        GPIO.output(x[1], GPIO.LOW)                    #Set 1
         msd_mqtt(str(x[0]), 'ON')                       # send msg MQTT
         time.sleep(x[2])                                #
         print('zamkn gpio :'+ str(x[1]))
-        GPIO.output(x[1], GPIO.LOW)
+        GPIO.output(x[1], GPIO.HIGH)
         print('zamkniety: '+ str(x[0]))
         logging.info('zamkniety : '+ str(x[0]))
         msd_mqtt(str(x[0]), 'OFF')
@@ -60,17 +65,45 @@ def time_now():
     now = datetime.datetime.now()
     time_now_HMS = datetime.time(now.hour, now.minute)
 #    print(time_now_HMS)
-    time.sleep(1)
+#    time.sleep(1)
     return time_now_HMS
 start_time = time_now()
 print('Start APP : ' + str(start_time))
 
+### sunset geting from site meteocast.net hour of sunset and returning in format HH MM.
+def sunset():
+    try:
+        resp = requests.get(meteocast_url)                    #url in secrets 
+        soup = bs.BeautifulSoup(resp.text, 'html.parser')
+        table = soup.find('a', {'class':'wdn'})
+        hour = table.findAll('b')[2].text
+        HH = hour[0]+hour[1]
+        MM = hour[3]+hour[4]
+        time.sleep(60)                                    #sleep to make req only once
+        return int(HH), int(MM)
+        logging.info('Zachod słonca o: {}:{}'.format(HH, MM))
+    except:
+        print('sunset() error')
+        logging.info('sundet() error')
+
+###GPIO reload sequence on start  
+for i in valves:
+        GPIO.setup(i[1], GPIO.OUT)
+        GPIO.output(i[1], GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(i[1], GPIO.HIGH)
+        time.sleep(1)
+
+
 while True:
     now = datetime.datetime.now()
     zm = datetime.time(now.hour, now.minute, now.second)
+
 #    print(zm)
     time.sleep(1)
-
+    if time_now() == datetime.time(21, 3):              #sunset update time
+            HH, MM = sunset()
+            print(HH, MM)      
 #    print('od poczatku')
     while GPIO.input(3) == 0:
 #    while gpio_rain == 1:                              # only for testing
@@ -93,7 +126,7 @@ while True:
                 print('ziemia wilgotna '+ str(delay_time))
         else:
 
-                if time_now() == datetime.time(13,18): #Setting when watering starts
+                if time_now() == datetime.time(HH,MM): #Setting when watering starts
                         start_time = time_now()
                         print ('Zaczynam Podlewanie: ' + str(start_time) )
                         watering()
